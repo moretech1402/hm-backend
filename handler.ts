2572bac08be49df8fc5f18dfc53e2ba11b3e7ec7
Carlos Moreno <morenort1402@gmail.com>
2025-01-28 12:23:44 +0100

    [feat] new github action

--- handler.ts
+++ handler.ts
@@ -2,5 +2,16 @@ import serverless from 'serverless-http';
 import app from './src/app.js';
 
 export const handler = serverless(app, {
-	response: { headers: { 'Access-Control-Allow-Origin': '*' } }
+	response: { 	
+        headers: {
+          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Credentials': true,
+        } }
 })
+
+export const hello = async () => {
+    return {
+      statusCode: 200,
+      body: JSON.stringify({ message: 'Hello, World!' })
+    };
+  };

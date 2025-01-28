3e9d2215cae78b56e48792569f21d2e0ddc77f4f
Carlos Moreno <morenort1402@gmail.com>
2025-01-28 10:26:16 +0100

    [fix] cors in handler

--- handler.ts
+++ handler.ts
@@ -1,4 +1,6 @@
 import serverless from 'serverless-http';
 import app from './src/app.js';
 
-export const handler = serverless(app);
+export const handler = serverless(app, {
+	response: { headers: { 'Access-Control-Allow-Origin': '*' } }
+})

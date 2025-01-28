daead32f16fcfcdd829fd8ee2c6f68021a27e1b4
Carlos Moreno <morenort1402@gmail.com>
2025-01-28 11:38:35 +0100

    [fix] cors

--- src/app.ts
+++ src/app.ts
@@ -12,7 +12,13 @@ import { endpoints } from './app/api';
 
 const app = express();
 
-app.use(cors());
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    allowedHeaders: ['Content-Type', 'Authorization'],
+    credentials: true,
+  }));
+  
 app.use(express.json());
 app.use(cookieParser());
 
